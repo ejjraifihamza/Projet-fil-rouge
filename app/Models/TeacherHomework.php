37c0779afb0281ccb8304c7bdaed816eefd910ff
ejjraifihamza <ejjraifihamza@gmail.com>
2021-07-27 21:58:55 +0100

    🔧 Create TeacherHomework model

---
+++ app/Models/TeacherHomework.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TeacherHomework extends Model
+{
+    use HasFactory;
+
+    protected $table = 'teacher_homework'; 
+    protected $primaryKey = 'id';
+
+    protected $fillable = ['name', 'description', 'deadline', 'file_path'];
+}
