a6d0ceb620bf24bc9d1799cd8a853435eb12f26a
ejjraifihamza <ejjraifihamza@gmail.com>
2021-08-02 11:48:56 +0100

    💄 Create home page for cours

--- resources/views/teacher/home.blade.php
+++ resources/views/teacher/home.blade.php
@@ -48,7 +48,7 @@
     <div class="m-auto w-4/5 py-24">
         <div class="text-center">
             <h1 class="text-5xl uppercase bold">
-                cours
+                homework
             </h1>
         </div>
         <div class="pt-10">
