cea6466fb70478b5f2dab34054eeecc0d47fdc1d
ejjraifihamza <ejjraifihamza@gmail.com>
2021-08-03 11:39:41 +0100

    🚀 Update users table
    - Add class_name_id column

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,18 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->unsignedInteger('class_name_id');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->foreign('class_name_id')
+                ->references('id')
+                ->on('classes')
+                ->onDelete('cascade');
         });
     }
 
