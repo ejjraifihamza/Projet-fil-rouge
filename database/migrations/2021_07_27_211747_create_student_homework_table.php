8f631912ad0d842313dc5a5a6f35224f7008fc77
ejjraifihamza <ejjraifihamza@gmail.com>
2021-08-03 11:46:36 +0100

    🚀 Update student_homework table
    - Change id to increments
    - Change student_id column to user_id
    - Add teacher_homework_id and class_name_id column

--- database/migrations/2021_07_27_211747_create_student_homework_table.php
+++ database/migrations/2021_07_27_211747_create_student_homework_table.php
@@ -14,11 +14,25 @@ class CreateStudentHomeworkTable extends Migration
     public function up()
     {
         Schema::create('student_homework', function (Blueprint $table) {
-            $table->id();
-            $table->string('student_id');
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('teacher_homework_id')->unsigned();
+            $table->integer('class_name_id')->unsigned();
             $table->string('name');
             $table->string('file_path');
             $table->timestamps();
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+            $table->foreign('teacher_homework_id')
+                ->references('id')
+                ->on('teacher_homework')
+                ->onDelete('cascade');
+            $table->foreign('class_name_id')
+                ->references('id')
+                ->on('classes')
+                ->onDelete('cascade');
         });
     }
 
