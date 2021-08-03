d3dfb8717f34b5ad23a84e1755d6611e0f343e1a
ejjraifihamza <ejjraifihamza@gmail.com>
2021-08-03 15:24:16 +0100

    💄 Change addmanager form

--- resources/views/manager/addmanager.blade.php
+++ resources/views/manager/addmanager.blade.php
@@ -4,14 +4,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="icon" 
+      type="image/png"
+      sizes="32x32"
+      href="{{ asset('img/favicon.png') }}">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/> 
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>M-sab Education</title>
 </head>
 <body>
+    <header>
+        <div class="header-2">
+            <nav class="bg-white py-2 md:py-4">
+              <div class="container px-4 mx-auto md:flex md:items-center">
+                    <div class="flex justify-between items-center">
+                    <a href="/" class="font-bold text-xl text-indigo-600">M-sab Education</a>
+                    <button class="border border-solid border-gray-600 px-3 py-1 rounded text-gray-600 opacity-50 hover:opacity-75 md:hidden" id="navbar-toggle">
+                        <i class="fas fa-bars"></i>
+                    </button>
+                    </div>
+          
+                    <div class="hidden md:flex flex-col md:flex-row md:ml-auto mt-3 md:mt-0" id="navbar-collapse">
+                    <span class="p-2 lg:px-4 md:mx-2 text-gray-600 rounded hover:bg-gray-200 hover:text-gray-700 transition-colors duration-300">{{ Auth::user()->name }}</span>
+                    <a class="p-2 lg:px-4 md:mx-2 text-white rounded bg-indigo-600" href="{{ route('manager.logout') }}">Logout</a>
+                    </div>
+              </div>
+            </nav>
+        </div>
+    </header>
     <form action="{{ route('manager.uploadmanager') }}" method="post">
         @csrf
-        <input type="text" name="name">
-        <input type="text" name="email">
-        <input type="password" name="password">
+        <input type="text" name="name" placeholder="add name">
+        <input type="text" name="email" placeholder="add email">
+        <input type="password" name="password" placeholder="add password">
         <input type="submit" name="submit" id="">
         </form>
 </body>
