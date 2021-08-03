57e9d00b2d95cdf2f15ff828885d5c1114142997
ejjraifihamza <ejjraifihamza@gmail.com>
2021-08-03 11:37:05 +0100

    🚀 Create teacher_correct table

---
+++ database/migrations/2021_08_03_100445_create_teacher_corrects_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTeacherCorrectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teacher_corrects', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('teacher_id');
+            $table->unsignedInteger('student_homework_id');
+            $table->string('note');
+            $table->string('notice');
+            $table->timestamps();
+            $table->foreign('teacher_id')
+                ->references('id')
+                ->on('teachers')
+                ->onDelete('cascade');
+            $table->foreign('student_homework_id')
+                ->references('id')
+                ->on('student_homework')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teacher_corrects');
+    }
+}
