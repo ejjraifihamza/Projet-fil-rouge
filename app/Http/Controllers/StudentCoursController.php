92a8d60ee32b4df7961e93dc7e978c9e65696a4b
ejjraifihamza <ejjraifihamza@gmail.com>
2021-08-07 15:25:24 +0100

    🚀 update studentCoursController
    - Add function for arabic page
    - Add function for francais page
    - Add function for anglais page
    - Add function for math page

--- app/Http/Controllers/StudentCoursController.php
+++ app/Http/Controllers/StudentCoursController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\TeacherCours;
+use Illuminate\Support\Facades\Auth;
 
 class StudentCoursController extends Controller
 {
     public function index() {
-        $cours = TeacherCours::all();
+        $cours = TeacherCours::all()->where('class_name_id', '=', Auth::user()->class_name_id)
+                                    ->sortByDesc('created_at');
         return view('user.cours.home', [
             'cours' => $cours
         ]);
@@ -23,5 +25,42 @@ class StudentCoursController extends Controller
 
     public function download($file) {
         return response()->download(public_path('coursassets/'.$file));
+    }
+
+    public function arabic() {
+        $courses = TeacherCours::all()->where('subject', '=', 'arab')
+                                    ->where('class_name_id', '=', Auth::user()->class_name_id)
+                                    ->sortByDesc('created_at');
+        // dd($courses);
+        return view('user.cours.arabic', [
+            'courses' => $courses
+        ]);
+    }
+
+    public function francais() {
+        $courses = TeacherCours::all()->where('subject', '=', 'francais')
+                                    ->where('class_name_id', '=', Auth::user()->class_name_id)
+                                    ->sortByDesc('created_at');
+        return view('user.cours.francais', [
+            'courses' => $courses
+        ]);
+    }
+
+    public function anglais() {
+        $courses = TeacherCours::all()->where('subject', '=', 'anglais')
+                                    ->where('class_name_id', '=', Auth::user()->class_name_id)
+                                    ->sortByDesc('created_at');
+        return view('user.cours.anglais', [
+            'courses' => $courses
+        ]);
+    }
+
+    public function math() {
+        $courses = TeacherCours::all()->where('subject', '=', 'math')
+                                    ->where('class_name_id', '=', Auth::user()->class_name_id)
+                                    ->sortByDesc('created_at');
+        return view('user.cours.math', [
+            'courses' => $courses
+        ]);
     }
 }
