0ec8ef9991d9160ce6aa07b257f431fba5253606
ejjraifihamza <ejjraifihamza@gmail.com>
2021-08-07 15:19:54 +0100

    🚀  Create Eloquent relationship

--- app/Models/Teacher.php
+++ app/Models/Teacher.php
@@ -11,4 +11,16 @@ class Teacher extends Authenticatable
     use HasFactory;
 
     protected $guarded = [];
+
+    public function teacherTeacherCours() {
+        return $this->hasMany(TeacherCours::class);
+    }
+
+    public function teacherTeacherHomeworks() {
+        return $this->hasMany(TeacherHomework::class);
+    }
+
+    public function classe() {
+        return $this->belongsTo(Classe::class);
+    }
 }
