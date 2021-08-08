e63faf868c06a7c5aa077475749feaf33666ddca
ejjraifihamza <ejjraifihamza@gmail.com>
2021-08-08 23:14:18 +0100

    🚀 Add div for error alert message

--- resources/views/manager/login.blade.php
+++ resources/views/manager/login.blade.php
@@ -2,6 +2,29 @@
 
 
 @section('content')
+@if ($message = Session::get('error'))
+<div class="alert flex flex-row items-center bg-red-200 p-5 rounded border-b-2 border-red-300">
+  <div class="alert-icon flex items-center bg-red-100 border-2 border-red-500 justify-center h-10 w-10 flex-shrink-0 rounded-full">
+    <span class="text-red-500">
+      <svg fill="currentColor"
+         viewBox="0 0 20 20"
+         class="h-6 w-6">
+        <path fill-rule="evenodd"
+            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+            clip-rule="evenodd"></path>
+      </svg>
+    </span>
+  </div>
+  <div class="alert-content ml-4">
+    <div class="alert-title font-semibold text-lg text-red-800">
+      Error
+    </div>
+    <div class="alert-description text-sm text-red-600">
+      {{ $message }}
+    </div>
+  </div>
+</div>
+@endif
 
 <div class="flex space-x-32 justify-center bg-gray-50 py-36 px-4 sm:px-6 lg:px-8">
     <div class="max-w-md space-y-8">
